b68740efdb4c2abbb7c99d46913a80ee10ec5273
Felix <111078933+fbicuul@users.noreply.github.com>
2023-01-11 02:03:30 +0100

    Python - Input/Output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
-asvbnbnaa
+#!/usr/bin/python3
+"""load from json"""
+import json
+
+
+def load_from_json_file(filename):
+    """load from json"""
+    with open(filename, encoding="utf-8") as fd:
+        return json.load(fd)
